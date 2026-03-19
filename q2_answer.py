b28c86eca13dcb304ef2c16262b84df2f6c91f26
Janicelyh <janice_lyh@yahoo.com>
2026-03-19 15:59:36 +0800

    q2_answer.py

--- q2_answer.py
+++ q2_answer.py
@@ -16,13 +16,8 @@ def find_and_replace(lst, find_val, replace_val):
 Attempted answer : 
 
 def find_and_replace(lst, find_val, replace_val):
-    """
-    Create a function that searches for all occurrences of a value (find_val)
-    in a given list (lst) and replaces them with another value (replace_val).
-    - lst must be a list.
-    - Return the modified list.
-    """
-    if not isinstance(lst, list):
+    
+       if not isinstance(lst, list):
         raise TypeError("lst must be a list")
     
     return [replace_val if item == find_val else item for item in lst]
