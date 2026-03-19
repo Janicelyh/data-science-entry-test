4d5016e21f962c120e7e9ee1a63883a9a848f0e2
Janicelyh <janice_lyh@yahoo.com>
2026-03-19 16:00:36 +0800

    q4_answer.py

--- q4_answer.py
+++ q4_answer.py
@@ -16,11 +16,6 @@ def string_reverse(s):
 Attempted answer:
 
 def string_reverse(s):
-    """
-    - Create a function that reverses a given string (s).
-    - s must be a string.
-    - Return the reversed string.
-    """
     if not isinstance(s, str):
         raise TypeError("s must be a string")
     
