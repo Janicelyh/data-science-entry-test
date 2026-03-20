645124707e5cc42b51c35d84b6ec74c23f960f02
Janicelyh <janice_lyh@yahoo.com>
2026-03-20 12:24:47 +0800

    q6_answer.py

--- q6_answer.py
+++ q6_answer.py
@@ -16,24 +16,27 @@ def find_first_negative(lst):
 Attempted answers:
 
 def find_first_negative(lst):
-       i = 0
+    i = 0
     while i < len(lst):
         if lst[i] < 0:
             return lst[i]
         i += 1
-    
     return "No negatives"
-
 
 # Invoke the function with the given scenarios
 result1 = find_first_negative([3, 5, -1, 7, -2, 8])
 result2 = find_first_negative([2, 10, 7, 0])
 
-print(result1)
-print(result2)
+print(result1)  # Output: -1
+print(result2)  # Output: No negatives
 
 Output:
 -1
 No negatives
 
-
+Explainations: 
+The task 1 function allow us to do data validation to quickly detect the first negative numbers in a list without scanning the entire list. 
+This is useful in data cleaning where one can filter out outliers or values that doesnt make sense within the datasetss. In the above coding funtions, 
+it calls out negative values as the "error" data and users will be prompted to cleamn up the data. 
+The invoke fuctions calls out the secenarios to check on  negatives within the datasets where result1 calls out the negative value with -1 while 
+there is no negatives in result2.
